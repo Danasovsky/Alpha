0b5939b1547fc37386be798b2720c2523b3496ec
Danasovsky <75419773+Danasovsky@users.noreply.github.com>
2020-12-03 16:33:02 +0100

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,8 +1,9 @@
+// typewriter
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 3000;
+        this.period = parseInt(period, 10) || 2000;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
@@ -47,10 +48,21 @@ var TxtType = function(el, toRotate, period) {
             if (toRotate) {
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
-        }
+        };
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+    
+
+// menu icon
+
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
